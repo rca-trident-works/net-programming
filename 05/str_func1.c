9e7121a1854355edb9b99dd63f621c339e6bbcf1
rca <_@r0m.me>
2025-05-27 15:52:25 +0900

    format

--- 05/str_func1.c
+++ 05/str_func1.c
@@ -5,8 +5,8 @@ void print(char[]);
 int main(int argc, char *argv[]) {
   char hello[] = "Hello, World";
 
-  print (hello);
-  
+  print(hello);
+
   return 0;
 }
 
@@ -14,6 +14,6 @@ void print(char str[]) {
   for (int i = 0; str[i] != '\0'; i++) {
     printf("str[%02d] : (0x%02x) %c\n", i, str[i], str[i]);
   }
-  
+
   printf("str : %s\n", str);
 }
