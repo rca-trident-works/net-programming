0ef7c5d13f9b1d7a5628a42a50cd8cae1321b50b
rca <_@r0m.me>
2025-05-27 14:21:53 +0900

    add compare.c

---
+++ 05/compare.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]) {
+  char str1[] = "ABC";
+  char str2[] = "ABCD";
+
+  bool match = true;
+
+  for (int i = 0; i < sizeof(str1); i++) {
+    if (str1[i] != str2[i]) {
+      match = false;
+      break;
+    }
+  }
+
+  if (match) {
+    printf("文字列は一致しています。\n");
+  } else {
+    printf("文字列は一致していません。\n");
+  }
+
+  return 0;
+}
+
