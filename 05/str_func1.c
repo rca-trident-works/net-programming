506c2869f0ac6b60199c3efcc39cd4fb25b2016c
rca <_@r0m.me>
2025-05-27 14:23:04 +0900

    add str_func1

---
+++ 05/str_func1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void print(char[]);
+
+int main(int argc, char *argv[]) {
+  char hello[] = "Hello, World";
+
+  print (hello);
+  
+  return 0;
+}
+
+void print(char str[]) {
+  for (int i = 0; str[i] != '\0'; i++) {
+    printf("str[%02d] : (0x%02x) %c\n", i, str[i], str[i]);
+  }
+  
+  printf("str : %s\n", str);
+}
