5c43074eb1833d69e4892d024977867137930957
rca <_@r0m.me>
2025-06-03 16:38:06 +0900

    rewrite

--- 06/prog1.c
+++ 06/prog1.c
@@ -15,10 +15,12 @@ int main(void) {
 
 int findMax(int *arr, int size) {
   int max = arr[0];
+
   for (int i = 1; i < size; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
+    if (*(arr + i) > max) {
+      max = *(arr + i);
     }
   }
+
   return max;
 }
