9a3ec87e2c7f6a1c74145fcf4a69e05f4a202f46
rca <_@r0m.me>
2025-06-24 14:55:51 +0900

    add for_2_inf.c

---
+++ 03/for_2_inf.c
@@ -0,0 +1,18 @@
+// for_2.cの一部をコメントアウトして無限ループさせたバージョン
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int i = 0;
+
+  for(;;) {
+    /* if (i++ >= 5) { */
+    /*   break; */
+    /* } */
+    printf("%d回目のループです\n", i);
+  }
+
+  printf("for文の後に実行されます\n");
+
+  return 0;
+}
