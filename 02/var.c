bff0cd4a0e76e74674b55f2efe6356d23bf95b4e
rca <_@r0m.me>
2025-06-24 14:15:00 +0900

    add var.c

---
+++ 02/var.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int a;        // aを宣言
+  a = 1;        // aに1を代入
+  int b = 1;    // bを宣言し、1を代入
+  int c, d = 1; // cとdを宣言し、dに1を代入
+
+  return 0;
+}
