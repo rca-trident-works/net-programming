f6602607a6b38a35244719b7c3cb1028184c2750
rca <_@r0m.me>
2025-07-01 16:10:33 +0900

    impl delete

---
+++ 09/list4.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+  int data;
+  struct Node *next;
+} Node;
+
+Node *createNode(int);
+void append(Node *, Node *);
+void display(Node *);
+void prepend(Node **, Node *);
+void insert(Node **, Node *, int);
+void delete(Node **, int);
+
+int main(int argc, char const *argv[]) {
+  Node *head = NULL;
+  Node *data;
+
+  head = createNode(100);
+  display(head);
+
+  printf("== append ==\n");
+
+  data = createNode(200);
+  append(head, data);
+
+  data = createNode(300);
+  append(head, data);
+  display(head);
+
+  printf("== prepend ==\n");
+
+  data = createNode(400);
+  prepend(&head, data);
+  display(head);
+
+  printf("== insert ==\n");
+
+  data = createNode(500);
+  insert(&head, data, 300);
+
+  data = createNode(600);
+  insert(&head, data, 999);
+  display(head);
+
+  printf("== delete ==\n");
+
+  delete(&head, 200);
+  delete(&head, 400); // 先頭ノードの削除は行わせない
+  delete(&head, 999); // markerが見つからない場合は何もしない
+
+  display(head);
+
+  return 0;
+}
+
+Node *createNode(int data) {
+  Node *newNode = (Node *)malloc(sizeof(Node));
+  newNode->data = data;
+  newNode->next = NULL;
+  return newNode;
+}
+
+void append(Node *head, Node *data) {
+  Node *current = head;
+  while (current->next != NULL) {
+    current = current->next;
+  }
+  current->next = data;
+}
+
+void prepend(Node **head, Node *data) {
+  data->next = *head;
+  *head = data;
+}
+
+void insert(Node **head, Node *data, int marker) {
+  Node *current = *head;
+  Node *prev = NULL;
+  while (current != NULL && current->data != marker) {
+    prev = current;
+    current = current->next;
+  }
+  if (current != NULL) {
+    data->next = current->next;
+    current->next = data;
+  } else {
+    prev->next = data;
+  }
+}
+
+void delete(Node **head, int marker) {
+  Node *current = *head;
+  Node *prev = NULL;
+  while (current != NULL && current->data != marker) {
+    prev = current;
+    current = current->next;
+  }
+  if (current == *head) {
+    // 先頭ノードの削除は行わせない
+    return;
+  } else if (current != NULL && prev != NULL) {
+    prev->next = current->next;
+    free(current);
+  } else {
+    // markerが見つからない場合は何もしない
+    return;
+  }
+}
+
+void display(Node *head) {
+  Node *current = head;
+  printf("現在のアドレス\t次のアドレス\t値\n");
+  while (current != NULL) {
+    printf("%p\t%p\t%d\n", current, current->next, current->data);
+    current = current->next;
+  }
+  printf("\n");
+}
