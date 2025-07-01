0094edfe456779bd13b2ab507a167012de4a984c
rca <_@r0m.me>
2025-07-01 15:37:35 +0900

    add list1.c

---
+++ 09/list1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+  int data;
+  struct Node *next;
+} Node;
+
+Node *createNode(int);
+void append(Node *, Node *);
+void display(Node *);
+
+int main(int argc, char const *argv[]) {
+  Node *head = NULL;
+  Node *data;
+
+  head = createNode(100);
+  display(head);
+
+  data = createNode(200);
+  append(head, data);
+
+  data = createNode(300);
+  append(head, data);
+
+  display(head);
+
+  return 0;
+}
+
+Node *createNode(int data) {
+  Node *newNode = (Node *)malloc(sizeof(Node));
+  newNode->data = data;
+  newNode->next = NULL;
+  return newNode;
+}
+
+void append(Node *head, Node *data) {
+  Node *current = head;
+  while (current->next != NULL) {
+    current = current->next;
+  }
+  current->next = data;
+}
+
+void display(Node *head) {
+  Node *current = head;
+  printf("現在のアドレス\t次のアドレス\t値\n");
+  while (current != NULL) {
+    printf("%p\t%p\t%d\n", current, current->next, current->data);
+    current = current->next;
+  }
+  printf("\n");
+}
