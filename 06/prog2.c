7bb5795dbb2a0222e4103c2217e40f3661fe8d64
rca <_@r0m.me>
2025-08-05 14:04:00 +0900

    impl 06/prog2.c

--- 06/prog2.c
+++ 06/prog2.c
@@ -1,3 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
+void reverseString(char *str);
+
+int main() {
+  char message[] = "Hello, world!";
+
+  printf("Before: %s\n", message);
+  reverseString(message);
+  printf("After: %s\n", message);
+
+  return 0;
+}
+
+void reverseString(char *str) {
+  int length = strlen(str);
+  for (int i = 0; i < length / 2; i++) {
+    char temp = *(str + i);
+    *(str + i) = *(str + length - 1 - i);
+    *(str + length - 1 - i) = temp;
+  }
+}
